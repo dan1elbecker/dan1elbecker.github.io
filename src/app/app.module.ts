0413fde78679ce83e309a4cd9a4097f9bc844b26
Daniel Programming <danielprogramming@mbp-von-daniel.speedport.ip>
2023-04-03 22:41:39 +0200

    initialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { LandingContentComponent } from './landing-content/landing-content.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { DatenschutzComponent } from './datenschutz/datenschutz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TestimonialComponent,
+    LandingContentComponent,
+    AboutMeComponent,
+    ImpressumComponent,
+    DatenschutzComponent
   ],
   imports: [
     BrowserModule,
